792be1750bfe4add949e1e07b36ff3343c117252
AndresNoeg <a.23308051281184@cbtis128.edu.mx>
2024-09-23 20:51:17 -0600

    Practica 5 Adivina

---
+++ Adivina.py
@@ -0,0 +1,17 @@
+print (" ")
+print ("Andres Noe Gomez Garcia Adivina")
+print(" ")
+
+#Valor a adivinar
+x=3
+#Se le pide un numero al usuario 
+n1=int(input("Estoy pensando en un numero Soy un número pequeño, pero tengo mucha historia,en la triada y en el arte,también en la memoria.¿Qué número soy? "))
+
+
+if n1==x:#Si el numero del usuario es igual al valor 
+    print(" ")#imprimira esto
+    print("¡Felicidades Haz adivinado!")
+else:#Si no es igual
+    print(" ")#Imprimira esto
+    print("Ese no es el numero; ¡Pero puedes seguir intentando!")
+print (" ")
